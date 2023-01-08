fb779fe8b63b027a5378c454facf9808b8622959
hayoung0Lee <hayoung0.lee@gmail.com>
2023-01-08 21:08:31 +0900

    Removed build folder

--- build/CMakeFiles/MyProject.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/MyProject.dir/src/main.cpp.o: \
-  /Users/hayoung/chatGPT/game_1/src/main.cpp \
-  /opt/homebrew/Cellar/raylib/4.2.0/include/raylib.h \
-  /Library/Developer/CommandLineTools/usr/lib/clang/13.0.0/include/stdarg.h
